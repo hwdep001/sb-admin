be14dc92dc78a14b8747ea39d73168cb4d378082
howoh <hwdep001@gmail.com>
2017-10-24 10:29:09 +0900

    init

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { routerTransition } from '../router.animations';
+import * as firebase from 'firebase'
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    animations: [routerTransition()]
+})
+export class LoginComponent implements OnInit {
+
+    user;
+
+    constructor(public router: Router) {
+        this.onAuthStateChanged();
+    }
+
+    ngOnInit() {
+    }
+
+
+    onAuthStateChanged() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.user = user;
+            
+        });
+    }
+
+    onLoggedin() {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+
+    }
+
+    onSignOut() {
+        firebase.auth().signOut();
+    }
+
+}
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,16 @@
+import { environment } from './../../environments/environment';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        LoginRoutingModule
+    ],
+    declarations: [LoginComponent]
+})
+export class LoginModule {
+}
